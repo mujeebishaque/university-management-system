56936cfb3ae2142f4807ef061ce588dc64229289
mujeebishaque <mujeeb.ishaque@gmail.com>
2019-06-28 16:30:13 +0500

    added headlines model

--- headlines/admin.py
+++ headlines/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from headlines import models
 # Register your models here.
+class HeadlineAdmin(admin.ModelAdmin):
+
+    list_display = ['headline', 'dateposted']
+
+admin.site.register(models.Headline)
